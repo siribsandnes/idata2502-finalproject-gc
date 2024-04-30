658b99eb785f41e97986cde6df0b7d8627bef4bf
Siri Sandnes <siri.b.sandnes@gmail.com>
2024-04-30 23:36:04 +0200

    bug fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("var.google_application_credentials")
+  credentials = file(var.google_application_credentials)
   project     = "brave-aviary-400922"
   region      = "us-central1"
 }
