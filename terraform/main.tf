724f81f076f95be5446f66df18d6963a5167337d
Siri Sandnes <siri.b.sandnes@gmail.com>
2024-05-01 11:21:29 +0200

    test new server

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "default" {
-  name         = "web-server-1"
+  name         = "web-server-2"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
   tags         = ["http-server"]
